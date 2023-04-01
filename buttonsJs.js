c3cfb15ea9a1a246ac42eecb73185bfc8fbccddc
Nick H <113550184+NicKreb@users.noreply.github.com>
2023-03-31 21:39:17 -0500

    Update buttonsJs.js

--- buttonsJs.js
+++ buttonsJs.js
@@ -1,1 +1,4 @@
 /* here will reside the JS for the project, primarily filled with the functions of the 2nd and 3rd buttons! */
+var datetime = new Date();
+console.log(datetime);
+document.getElementById("time").textContent = datetime;
