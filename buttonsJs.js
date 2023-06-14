d61d1ded19480c63beed14b8df74ab0be628af4a
Nick H <113550184+NicKreb@users.noreply.github.com>
2023-06-14 14:27:53 -0500

    Update buttonsJs.js

--- buttonsJs.js
+++ buttonsJs.js
@@ -3,5 +3,8 @@ var datetime = new Date();
 console.log(datetime);
 document.getElementById("time").textContent = datetime;
 
+function displayBoxResults() {
+  
+};
 //create a conditional function where the display value changes for each button, and if one button is displaying, then pressing another one will switch the current display off
 //and the newest display on!
