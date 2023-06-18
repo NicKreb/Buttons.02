213852d3ff5a6e91f45b03cd24c1c4971d1c9196
Nick H <113550184+NicKreb@users.noreply.github.com>
2023-06-18 16:09:21 -0500

    Update buttonsJs.js
    started the function for the display box's results for each button press based on the value of 'displayVar'

--- buttonsJs.js
+++ buttonsJs.js
@@ -3,8 +3,17 @@ var datetime = new Date();
 console.log(datetime);
 document.getElementById("time").textContent = datetime;
 
-function displayBoxResults() {
+var displayVar = "blank";
+
+function displayBoxResults(displayVar) {
   
+  if (displayVar = "blank") {
+    document.getElementById("displayBox").innerHTML = "<h1> Display Results Here! </h1>";
+  } else if (displayVar = "time") {
+    document.getElementById("displayBox").innerHTML = "<h1>" + {dateTime} + "</h1>";
+  } else if (displayVar = "Pie") {
+    document.getElementById("displayBox").innerHTML = "<h1> 3.14 </h1>"
+  }
 };
 //create a conditional function where the display value changes for each button, and if one button is displaying, then pressing another one will switch the current display off
 //and the newest display on!
