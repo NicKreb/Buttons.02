643ae31fc8f29da1411bd7cd0511a7db00cd0bc5
Nick H <113550184+NicKreb@users.noreply.github.com>
2023-06-20 13:59:04 -0500

    Update buttonsJs.js
    created the functions for most of the buttons. The only two left are the eraser button which should set everything to default, and the yin yang which should change the happy button to a sad button.

--- buttonsJs.js
+++ buttonsJs.js
@@ -1,24 +1,65 @@
 /* here will reside the JS for the project, primarily filled with the functions of the 2nd and 3rd buttons! */
 var dateTime = new Date();
 
-x = '';
+var dateTime = new Date();
+//it will print on html page
 
-function displayVarValue(x){
-let displayVar = ""
+//let display = document.getElementById("displayBox"); 
+
+//x = '';
+
+// function displayVarValue(x){
+// let display = displayVar;
+// var displayVar = x;
   
-if (x = 'Cl') {
-  displayVar = dateTime
-} else if (x = 'Sm') {
-  displayVar = "You Can Do Anything!"
-} else if (x = 'Pie') {
-  displayVar = "3.14"
-} else if (x = 'Er') {
-  displayVar = "Click a Button!"
-} else {
-  return displayVar; 
-}
+// if (x = 'Cl') {
+//   displayVar = dateTime
+// } else if (x = 'Sm') {
+//   displayVar = "You Can Do Anything!"
+// } else if (x = 'Pie') {
+//   displayVar = "3.14"
+// } else if (x = 'Er') {
+//   displayVar = "CLEARED"
+// } else {
+//   return displayVar; 
+// }
+// };
+
+// document.getElementById("clock-icon").onclick = function() {tellTime()};
+
+// function tellTime(){
+//  document.getElementById("clickaButton").innerHtml = "<h1>" + "Today is Today!" + "</h1>";
+// };
+
+//console.log(displayVarValue(x));
+
+document.getElementById("clock-icon").onclick = function() {tellTime()};
+document.getElementById("smile-icon").onclick = function() {smileMotherFucker()};
+document.getElementById("applepie-icon").onclick = function() {pi()};
+document.getElementById("color-icon").onclick = function() {changeBackground()};
+let tasteTheRainbow = ["red", "orange", "yellow", "green", "blue", "purple", "black"]
+
+function tellTime() {
+  document.getElementById("clickaButton").innerHTML = dateTime;
 };
 
-console.log(displayVarValue(x));
+function smileMotherFucker() {
+  document.getElementById("clickaButton").innerHTML = "YOU ARE DOING AMAZING!";
+};
+
+function pi() {
+  document.getElementById("clickaButton").innerHTML = Math.PI;
+};
+
+function changeBackground(color) {
+   document.body.style.background = tasteTheRainbow[Math.floor(Math.random() * tasteTheRainbow.length)]
+};
+
+//document.getElementById("yinyang-icon").onclick = function() {happyToSad()};
+
+// function happyToSad() {
+//   document.getElementById("clickaButton").innerHTML =;
+// };
+
 //create a conditional function where the display value changes for each button, and if one button is displaying, then pressing another one will switch the current display off
 //and the newest display on!
